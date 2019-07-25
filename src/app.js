3a7821605a38eaf3ec0e5765ce36037736166847
Ito Zapata <zaphyr@gmail.com>
2019-07-24 22:34:39 -0700

    lol forgot require

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const awsServerlessMiddleware = ("aws-serverless-express/middleware");
+const awsServerlessMiddleware = require("aws-serverless-express/middleware");
 
 const app = express();
 const router = express.Router();
